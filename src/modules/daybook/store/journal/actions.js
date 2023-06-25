faf464901d08952ebc1818e72acf92ed02af1cca
Bill Bravo <bbravo@ec.krugercorp.com>
2023-06-25 17:35:29 -0500

    first commit

---
+++ src/modules/daybook/store/journal/actions.js
@@ -0,0 +1,7 @@
+// export const myAction = async ({ commit }) => {};
+
+// export const loadEntries = async ({ commit }) => {};
+
+// export const updateEntrie = async ({ commit }) => {};
+
+// export const createEntrie = async ({ commit }) => {};
---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,11 @@
+// export const myGetter = (state) => {
+//   return state.something;
+// };
+
+export const getEntriesByTerm = (state) => {
+  return state.something;
+};
+
+export const getEntryById = (state) => {
+  return state.something;
+};
